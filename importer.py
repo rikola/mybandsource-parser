631b1c02f7cd136b53239a6e4fc9ab6199a2b246
Eric Koehler <rikola2@gmail.com>
2017-02-07 02:05:45 -0600

    Separates different social media links into their own DB columns.

--- importer.py
+++ importer.py
@@ -3,21 +3,66 @@ import xml.etree.ElementTree as ET
 import psycopg2 as p
 from urllib.parse import urlparse
 
+
 class Artist:
 
-    def init(self, name, realname, profile, data_quality, urls):
-        self.name = name
-        self.realname = realname
-        self.profile = profile
-        self.data_quality = data_quality
-        self.urls = urls
+    def __init__(self):
+        self.id = None
+        self.name = None
+        self.realname = None
+        self.profile = None
+        self.data_quality = None
+        self.urls = []
+        self.namevariation = []
+        self.facebook_url = None
+        self.soundcloud_url = None
+        self.spotify_url = None
+        self.twitter_url = None
+        self.instagram_url = None
 
-    def print(self):
-        print(self.name, self.data_quality)
+
+    def parse_urls(self):
+
+        for url in self.urls:
+
+            o = ''
+            try:
+                o = urlparse(url)
+            except:
+                continue
+
+            try:
+                if o.netloc.find('facebook.com') >= 0:
+                    self.facebook_url = url
+            except:
+                self.report_error(url)
+            try:
+                if o.netloc.find('soundcloud.com') >= 0:
+                    self.soundcloud_url = url
+            except:
+                self.report_error(url)
+            try:
+                if o.netloc.find('spotify.com') >= 0:
+                    self.spotify_url = url
+            except:
+                self.report_error(url)
+            try:
+                if o.netloc.find('twitter.com') >= 0:
+                    self.twitter_url = url
+            except:
+                self.report_error(url)
+            try:
+                if o.netloc.find('instagram.com') >= 0:
+                    self.instagram_url = url
+            except:
+                self.report_error(url)
+
+    def report_error(self, url):
+        print(f"Parse Error: Artist={artist.name}, {url}")
 
 
 class Urls:
-    def init(self, facebook, soundcloud, spotify, itunes, twitter):
+    def __init__(self, facebook, soundcloud, spotify, itunes, twitter):
         self.facebook = facebook
         self.soundcloud = soundcloud
         self.spotify = spotify
@@ -27,6 +72,7 @@ class Urls:
 
 # Load XML file into a tree in memory
 tree = ET.parse('data/artists.xml')
+print("Parse complete")
 root = tree.getroot()
 
 # Connect to database
@@ -35,42 +81,37 @@ cur = con.cursor()
 
 
 # Iterate over each artist in db
-for artist in root:
+for entry in root:
 
-    for elem in artist:
+    artist = Artist()
+
+    for elem in entry:
         if elem.tag == 'id':
-            pass
+            artist.id = int(elem.text)
         if elem.tag == 'name':
-            pass
+            artist.name = elem.text
         if elem.tag == 'realname':
-            pass
+            artist.realname = elem.text
         if elem.tag == 'profile':
-            pass
-        if elem.tag == 'quality':
-            pass
+            artist.profile = elem.text
         if elem.tag == 'data_quality':
-            pass
-        if elem.tag == 'namevariation':
-            pass
+            artist.data_quality = elem.text
+        if elem.tag == 'namevariations':
+            for name in elem:
+                artist.namevariation.append(name.text)
         if elem.tag == 'urls':
-            pass
+            for url in elem:
+                if url.text is not None:
+                    artist.urls.append(url.text)
 
-    # id = child.findtext('id')
-    # name = child.findtext('name')
-    # realname = child.findtext('realname')
-    # profile = child.findtext('profile')
-    # quality = child.findtext('data_quality')
-    # namevariations = child.findtext('namevariations')
-    #
-    # urls_block = child.find("urls")
-    # urls = list(urls_block.iter("url"))
-    #
-    # aliases = child.findtext('aliases')
+    artist.parse_urls()
 
     try:
-        cur.execute("INSERT INTO artists VALUES (%s, %s, %s, %s, %s)", (id, name, realname, profile, quality))
+        cur.execute("INSERT INTO artists VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", (artist.id,
+                                                                                                    artist.name,
+                                                                                                    artist.realname, artist.profile, artist.data_quality,
+                                                                                                    artist.namevariation, artist.facebook_url, artist.soundcloud_url, artist.spotify_url, artist.twitter_url, artist.instagram_url))
         con.commit()
-        print(name, realname, profile, quality, urls)
     except:
         con.rollback()
         print("Insert failed")
